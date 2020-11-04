30df23388b4f7a71abfbb1812233a6876354b726
Adrien MORY <adrien.mory@sogelink.fr>
2020-11-04 11:04:16 +0100

    Initial commit

---
+++ src/main/java/com/morysh/lawnmower/App.java
@@ -0,0 +1,13 @@
+package com.morysh.lawnmower;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
